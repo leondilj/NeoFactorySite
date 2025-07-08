de4f83f3df6938eb49370602d19363e4ed2cb6a0
Leondil Ribeiro <leondilribeiro@gmail.com>
2025-07-08 13:33:12 -0300

    Força novo deploy com projeto na raiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
@@ -8,9 +7,6 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
