04f6ce4f97d57b9aa1bd97a94831f9c158f19f85
Leondil Ribeiro <leondilribeiro@gmail.com>
2025-07-08 20:51:32 -0300

    Corrige base para build em domínio personalizado

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ✅ Corrige caminhos para produção
 export default defineConfig({
+  base: './',
   plugins: [react()],
 })
